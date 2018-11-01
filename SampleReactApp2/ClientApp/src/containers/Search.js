3c06cf5f14d5f9fa9945d8ab8871a260ac8996c1
rebeccahong <rebeccayhong@gmail.com>
2018-11-01 16:47:39 -0700

    Add project files.

---
+++ SampleReactApp2/ClientApp/src/containers/Search.js
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { Col, Grid, Row } from 'react-bootstrap';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Counter';
+
+class Search extends React.Component {
+
+    state = {
+        searchTerm: '',
+    };
+
+    handleChange = searchTerm => event => {
+        this.setState({
+            searchTerm: event.target.value,
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return ([
+            <Col sm={12}>
+                <TextField
+                    id="outlined-name"
+                    label="Search the OMDB API"
+                    value={this.state.searchTerm}
+                    onChange={this.handleChange('searchTerm')}
+                    margin="normal"
+                    variant="outlined"
+                />
+            </Col>,
+            <Col sm={12}>
+                {this.state.searchTerm}
+            </Col>
+        ]);
+    }
+
+}
+
+export default Search;
